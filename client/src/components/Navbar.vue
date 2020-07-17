282e9db8e2b2c85caaaa3ac5371a1caeea1db8a7
andysml <andy922200@gmail.com>
2020-07-17 11:12:55 +0800

    initialize navbar layout

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="navbar navbar-dark bg-dark">
+    <div class="button-group">
+      <button type="button" class="btn btn-outline-success">Log In</button>
+      <button type="button" class="btn btn-outline-danger">Log Out</button>
+      <button type="button" class="btn btn-outline-primary">Register</button>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
+
+<style></style>
--- client/src/views/Homepage.vue
+++ client/src/views/Homepage.vue
@@ -1,24 +1,36 @@
 <template>
-  <div class="homepage">
-    <h3>This is Homepage.</h3>
-    <b-form-group label="Individual radios">
-      <b-form-radio v-model="selected" name="some-radios" value="A"
-        >Option A</b-form-radio
-      >
-      <b-form-radio v-model="selected" name="some-radios" value="B"
-        >Option B</b-form-radio
-      >
-    </b-form-group>
+  <div class="container-outside-wrapper">
+    <Navbar />
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="homepage">
+            <h3>This is Homepage.</h3>
+            <b-form-group label="Individual radios">
+              <b-form-radio v-model="selected" name="some-radios" value="A"
+                >Option A</b-form-radio
+              >
+              <b-form-radio v-model="selected" name="some-radios" value="B"
+                >Option B</b-form-radio
+              >
+            </b-form-group>
 
-    <div class="mt-3">
-      Selected: <strong>{{ selected }}</strong>
+            <div class="mt-3">
+              Selected: <strong>{{ selected }}</strong>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Navbar from "./../components/Navbar";
+
 export default {
   name: "Homepage",
+  components: { Navbar },
   data() {
     return {
       selected: ""
